251ec73eccf038218cc506fdc41ad2c67b70dbd0
tuc0w <andreasbehrend@gmail.com>
2020-07-16 00:52:03 +0200

    Refactor database connection using isolates

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,8 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:NOVID_19/main.dart';
 
 void main() {
-    testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-        // Build our app and trigger a frame.
-        await tester.pumpWidget(MyApp());
+    testWidgets('Novid test', (WidgetTester tester) async {
+        await tester.pumpWidget(Novid19());
     });
 }
