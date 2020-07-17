ad806270d80216596a2bf6ef5c9575481853db6f
tuc0w <andreasbehrend@gmail.com>
2020-07-17 14:37:23 +0200

    Move distance calculation to math helper

--- lib/helper/math.dart
+++ lib/helper/math.dart
@@ -1,5 +1,22 @@
 import 'dart:math';
 
+/// Constants used for distance calculation.
+const num measuredPower = -72.0; // the expected RSSI at a distance of 1 meter
+const num broadcastingPower = 3.9; // the broadcasting power is around 2-4 dBm
+
+/// Calculates the approximate distance.
+///
+/// Distance = 10 ^ ((Measured Power - RSSI)/(10 * N))
+/// Measured Power = -75
+/// N = 3.9 (Consider high signal strength)
+/// ```dart
+/// calculateDistance(-80) == 1.34
+/// ```
+/// Returns the distance in meters.
+double calculateDistance(int rssi) {
+    return pow(10, ((measuredPower - (rssi))/(10 * broadcastingPower)));
+}
+
 double roundDouble(double value, int places){ 
     double mod = pow(10.0, places); 
     return ((value * mod).round().toDouble() / mod); 
